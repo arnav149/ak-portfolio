1cd50ae8a47399f5dff789bf2c0c54028c3e3760
Arnav Kasturia <arnavkasturia@hotmail.com>
2023-11-20 23:51:06 -0500

    add icons

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./AboutMe.css";
+import { FaInstagram, FaLinkedinIn } from "react-icons/fa";
 
 const AboutMe = () => {
   return (
@@ -13,6 +14,22 @@ const AboutMe = () => {
           I'm like a video game character - I level up by drinking coffee and
           eating fried chicken.
         </p>
+        <div className="social-icons">
+          <a
+            href="https://www.linkedin.com/in/arnav-kasturia/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedinIn />
+          </a>
+          <a
+            href="https://www.instagram.com/kasturiaarnav/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram />
+          </a>
+        </div>
       </div>
     </div>
   );
