c437a64b9486bfab0cd72f32d0aae3777cf6b8cd
Arnav Kasturia <arnavkasturia@hotmail.com>
2023-11-21 23:05:20 -0500

    add loop text

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,8 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  FaBed,
+  FaBug,
+  FaRedoAlt,
+  FaInstagram,
+  FaLinkedinIn
+} from "react-icons/fa";
+import { FaBowlFood } from "react-icons/fa6";
 import "./AboutMe.css";
-import { FaInstagram, FaLinkedinIn } from "react-icons/fa";
 
 const AboutMe = () => {
+  const elements = [
+    "Eat.",
+    <FaBowlFood key="food" />,
+    "\n",
+    "Sleep.",
+    <FaBed key="bed" />,
+    "\n",
+    "Debug.",
+    <FaBug key="bug" />,
+    "\n",
+    "Repeat.",
+    <FaRedoAlt key="redo" />,
+    "\n"
+  ];
+  const [index, setIndex] = useState(0);
+  const [visibleElements, setVisibleElements] = useState([]);
+
+  useEffect(() => {
+    if (index > elements.length) {
+      // Reset the index after the last element
+      setTimeout(() => {
+        setIndex(0);
+        setVisibleElements([]);
+      }, 2000); // Delay before restarting
+      return;
+    }
+
+    // Timer to update index
+    const timer = setTimeout(() => {
+      setVisibleElements(elements.slice(0, index));
+      setIndex(index + 1);
+    }, 150); // Adjust this value to control typing speed
+
+    return () => clearTimeout(timer);
+  }, [index]);
+
   return (
     <div className="about-me">
       <div className="profile-picture">
@@ -31,6 +74,7 @@ const AboutMe = () => {
           </a>
         </div>
       </div>
+      <div className="loop-text">{visibleElements}</div>
     </div>
   );
 };
