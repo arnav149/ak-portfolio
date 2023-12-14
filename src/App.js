103f0c50d11e449211c1e1efaf1355e14b999209
Arnav Kasturia <arnavkasturia@hotmail.com>
2023-12-13 19:01:52 -0500

    add experience route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
-import './App.css';
-import TerminalLoader from './TerminalLoader.js';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import AboutMe from './AboutMe';
-import NavBar  from './NavBar';
-import Education  from './Education';
+import React, { useState } from "react";
+import "./App.css";
+import TerminalLoader from "./TerminalLoader.js";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import AboutMe from "./AboutMe";
+import NavBar from "./NavBar";
+import Education from "./Education";
+import Experience from "./Experience";
 
 function App() {
   const [loadingComplete, setLoadingComplete] = useState(false);
@@ -14,26 +15,27 @@ function App() {
       setLoadingComplete(true);
     }, 1000);
   };
-    
+
   return (
     <div>
-      {!loadingComplete && 
-      <div className="loadingScreen">
-        <TerminalLoader onLoadingComplete={handleLoadingComplete} />
-      </div>
-      }
-      {loadingComplete && 
+      {!loadingComplete && (
+        <div className="loadingScreen">
+          <TerminalLoader onLoadingComplete={handleLoadingComplete} />
+        </div>
+      )}
+      {loadingComplete && (
         // Your actual app content goes here...
         <Router>
           <div className="appContent">
-              <NavBar />
-              <Routes>
-                  <Route path="/" element={<AboutMe />} />
-                  <Route path="/education" element={<Education />} />
-              </Routes>
+            <NavBar />
+            <Routes>
+              <Route path="/" element={<AboutMe />} />
+              <Route path="/education" element={<Education />} />
+              <Route path="/experience" element={<Experience />} />
+            </Routes>
           </div>
         </Router>
-      }
+      )}
     </div>
   );
 }
