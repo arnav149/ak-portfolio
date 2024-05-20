86458c6dd1a2484a91e4e792f611d7d8804e17cc
Arnav Kasturia <arnavkasturia@hotmail.com>
2024-05-19 21:34:00 -0400

    make it dark mode

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -52,7 +52,6 @@ const AboutMe = () => {
         <img src={process.env.PUBLIC_URL + "/profile_pic.jpg"} alt="Profile" />
       </div>
       <div className="content-card">
-        <h1>About Me</h1>
         <p>
           I'm like a video game character - I level up by drinking coffee and
           eating fried chicken.
